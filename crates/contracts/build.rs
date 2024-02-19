958e3453771f9afcafd4bfb3dfdd07343e3fc9cf
reptarrat <reptarrat@protonmail.com>
2024-02-19 14:23:54 -0500

    wip

--- crates/contracts/build.rs
+++ crates/contracts/build.rs
@@ -2,43 +2,35 @@ use std::env;
 use std::fs;
 use std::path::Path;
 use ethers_solc::{Project, ProjectPathsConfig};
+use ethers_solc::Solc;
 
 fn main() {
     // let out_dir = env::var_os("OUT_DIR").unwrap();
     // let dest_path = Path::new(&out_dir).join("hello.rs");
 
-	// let sol_files = vec![
-	// 	"./external/eigenlayer-middleware/src/BLSApkRegistry.sol",
-	// 	"./external/eigenlayer-middleware/src/BLSApkRegistryStorage.sol",
-	// 	"./external/eigenlayer-middleware/src/BLSSignatureChecker.sol",
-	// 	"./external/eigenlayer-middleware/src/IndexRegistry.sol",
-	// 	"./external/eigenlayer-middleware/src/IndexRegistryStorage.sol",
-	// 	"./external/eigenlayer-middleware/src/IndexRegistryRetriever.sol",
-	// 	"./external/eigenlayer-middleware/src/RegistryCoordinator.sol",
-	// 	"./external/eigenlayer-middleware/src/RegistryCoordinatorStorage.sol",
-	// 	"./external/eigenlayer-middleware/src/ServiceManagerBase.sol",
-	// 	"./external/eigenlayer-middleware/src/StakeRegistry.sol",
-	// 	"./external/eigenlayer-middleware/src/StakeRegistryStorage.sol",
-	// ];
+	let sol_files = vec![
+		"./external/eigenlayer-middleware/src/BLSApkRegistry.sol",
+		"./external/eigenlayer-middleware/src/BLSApkRegistryStorage.sol",
+		"./external/eigenlayer-middleware/src/BLSSignatureChecker.sol",
+		"./external/eigenlayer-middleware/src/IndexRegistry.sol",
+		"./external/eigenlayer-middleware/src/IndexRegistryStorage.sol",
+		"./external/eigenlayer-middleware/src/IndexRegistryRetriever.sol",
+		"./external/eigenlayer-middleware/src/RegistryCoordinator.sol",
+		"./external/eigenlayer-middleware/src/RegistryCoordinatorStorage.sol",
+		"./external/eigenlayer-middleware/src/ServiceManagerBase.sol",
+		"./external/eigenlayer-middleware/src/StakeRegistry.sol",
+		"./external/eigenlayer-middleware/src/StakeRegistryStorage.sol",
+	];
 
-	// Solc::default().async_compile_source("./external/eigenlayer-middleware/src/")
+	let compiler_output =
+		Solc::default().compile_source("./external/eigenlayer-middleware/src/").unwrap();
 
-	let project = Project::builder()
-        .paths(ProjectPathsConfig::hardhat("./external/eigenlayer-middleware/src/").unwrap())
-        .build()
-        .unwrap();
-    let output = project.compile().unwrap();
+	// for (name, contract) in compiler_output.contracts_into_iter() {
+	// 	let abi = contract.abi.unwrap();
+	// 	let json = serde_json::to_string(&abi).unwrap();
 
-    // Tell Cargo that if a source file changes, to rerun this build script.
-    project.rerun_if_sources_changed();
+	// 	fs::write(format!("src/gen/{name}.abi.json"), &json).unwrap();
+	// }
 
-    // fs::write(
-    //     &dest_path,
-    //     "pub fn message() -> &'static str {
-    //         \"Hello, World!\"
-    //     }
-    //     "
-    // ).unwrap();
 
-    // println!("cargo:rerun-if-changed=external/*");
 }
